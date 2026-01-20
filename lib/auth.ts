9711905a4aace2ce0eb6d4b853c32c1b57b84b78
Atharvjainn <jainatharv20@gmail.com>
2026-01-20 12:06:52 +0530

    configured better-auth

---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth} from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+    emailAndPassword : {
+        enabled : true,
+    },
+    socialProviders : {
+        github : {
+            clientId : process.env.GITHUB_CLIENT_ID as string,
+            clientSecret : process.env.GITHUB_CLIENT_SECRET as string ,
+        },
+        google : {
+            clientId : process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET as string,
+        }
+    },
+    plugins : [nextCookies()],
+    experimental : {joins : true}
+})
